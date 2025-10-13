755a0a4a31ee0c2af4b94400de2ec691379a3125
Arpita Shanbhag <axshanbhag@zonemail.clpccd.edu>
2025-10-13 11:14:06 -0700

    Create program1.cpp

---
+++ week2/program1.cpp
@@ -0,0 +1,18 @@
+
+
+// preprocessor directive
+#include <iostream>
+// namespace
+using namespace std;
+// main function that returns an integer
+int main() {
+// initalizes two numbers as variables
+int num1 = 50;
+int num2 = 100;
+// adds the two numbers and stores the result in a variable
+int total = num1 + num2;
+// prints the result to the console, followed by a new line
+cout << total << endl;
+// returns integer 0, the expected data type for main function
+return 0;
+}
