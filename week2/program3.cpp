e1c1f6ddb0e597d31aa6f9e7d55d3b6dc8787ffc
Arpita Shanbhag <axshanbhag@zonemail.clpccd.edu>
2025-10-13 11:14:50 -0700

    Create program3.cpp

---
+++ week2/program3.cpp
@@ -0,0 +1,16 @@
+
+
+// preprocessor directive
+#include <iostream>
+// namespace
+using namespace std;
+// main function that returns an integer
+int main() {
+// pattern using string literals, followed by new lines
+cout << " *" << endl;
+cout << " ***" << endl;
+cout << " *****" << endl;
+cout << "*******" << endl;
+// returns integer 0, the expected data type for main function
+return 0;
+}
